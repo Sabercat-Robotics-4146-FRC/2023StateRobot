1727e005075c8561e8924894c5f373b2f4b12a8f
noahkr07 <noahkeaneroth@gmail.com>
2023-09-15 11:56:36 -0700

    claw code

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -3,7 +3,10 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.revrobotics.*;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ClawConstants;
 
@@ -12,19 +15,28 @@ public class Claw extends SubsystemBase {
   private boolean clawEnabled;
 
   public Claw() {
-    clawMotor = new TalonFX(ClawConstants.CLAW_ID);
+    clawMotor = new CANSparkMax(ClawConstants.CLAW_ID, MotorType.kBrushless);
 
     clawEnabled = false;
+
+    clawMotor.setSmartCurrentLimit(80);
+    clawMotor.enableVoltageCompensation(12);
+    clawMotor.setOpenLoopRampRate(.5);
   }
 
-  public void toggleClaw(){
-    if(!clawEnabled) {
-        clawMotor.set(ControlMode.PercentOutput, 0);
-    } else {
-        clawMotor.set(ControlMode.PercentOutput, 0.6);
-    }
-
+  public void toggleClaw() {
     clawEnabled = !clawEnabled;
+    if(clawEnabled) clawMotor.setVoltage(6);
+    else clawMotor.stopMotor();
+  }
+
+  @Override
+  public void periodic() {
+      // if motor hits spike limit, lower voltage to hold in place
+      if(clawEnabled && clawMotor.getOutputCurrent() > 20) {
+        clawMotor.setVoltage(0.5);
+      }
+      SmartDashboard.putNumber("current", clawMotor.getOutputCurrent());
   }
 
 }
