39f9b340280a0117e84eb1f2968716e703677284
AdamSmithey <asmithey66@susdgapps.org>
2023-10-06 12:29:23 -0700

    merge

--- src/main/java/frc/robot/utils/CommandUtil.java
+++ src/main/java/frc/robot/utils/CommandUtil.java
@@ -1,34 +1,26 @@
 package frc.robot.utils;
 
-import java.lang.reflect.Constructor;
-
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.autos.AutoCommand;
 import frc.robot.RobotContainer;
 
 public class CommandUtil {
     private static CommandUtil instance;
     public static synchronized CommandUtil getInstance() {
-      if (instance == null) {
+    if (instance == null) {
         instance = new CommandUtil();
-      }
-      return instance;
+    }
+        return instance;
     }
     
     public Command getCommand(RobotContainer container, String str) {
         Command command = null;
         try {
             Class<?> c = Class.forName(str);
-            Constructor<?> con = c.getDeclaredConstructor(RobotContainer.class);
-            con.setAccessible(true);
-            command = (Command) con.newInstance(container);
+            command = (Command) c.getDeclaredConstructor(RobotContainer.class).newInstance(container);
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
-        
         
         return command;
     }
