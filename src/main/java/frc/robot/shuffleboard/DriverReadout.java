ec147d1353a942a3cbed66065b3d8c31b66aa1f3
AdamSmithey <asmithey66@susdgapps.org>
2023-10-13 11:30:04 -0700

    driver readout change

--- src/main/java/frc/robot/shuffleboard/DriverReadout.java
+++ src/main/java/frc/robot/shuffleboard/DriverReadout.java
@@ -6,24 +6,42 @@ import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import frc.robot.Constants.ArmConstants;
+import frc.robot.Constants.ArmConstants.ArmPositionConstants;
 
 public class DriverReadout {
     ShuffleboardTab tab;
 
     private GenericEntry team;
 
+    private int selected;
+    private String[] options;
+
     public DriverReadout() {
         tab = Shuffleboard.getTab("Main");
         
-        team = Shuffleboard.getTab("My Tab")
-            .add("My Number", 0)
+        team = Shuffleboard.getTab("Main")
+            .add("Team", 0)
             .withWidget(BuiltInWidgets.kComboBoxChooser)
             .withProperties(Map.of("Blue", "Blue", "Red", "Red"))
             .getEntry();
+
+        selected = 0;
+        options = new String[]{"Top","Middle","Bottom"};
+
+        tab.addString("Test", () -> options[selected]);
     }
 
     public String getTeam() {
         return team.getString("Red");
+    }
+
+    public ArmPositionConstants getSelectedArmPosition() {
+        return ArmConstants.ARM_POSITIONS.get(options[selected]);
+    }
+
+    public void setArmPosition(int dir) {
+        selected = Math.max(0, Math.min(selected+dir, 2));
     }
 
 }
