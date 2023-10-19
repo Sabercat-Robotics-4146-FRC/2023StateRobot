965634c26594c8a16ac0f0744d676015ee41d2ca
noahkr07 <noahkeaneroth@gmail.com>
2023-10-19 16:18:03 -0700

    init

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,6 +1,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -22,6 +23,7 @@ public class Robot extends TimedRobot {
     m_robotContainer = new RobotContainer();
 
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    DataLogManager.start();
   }
 
   @Override
